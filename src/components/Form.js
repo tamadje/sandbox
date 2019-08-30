28f69d14fe87f549a4044138fde211ae22fc7c47
patrick <ap34090@etsmtl.net>
2019-08-30 14:38:02 -0400

    creation formulaire

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
-const Form = ()=>{
-    return(
-        <div>Form component</div>
-    );
-};
+class Form extends React.Component{
+    state = {
+        name:'',
+        quantity:0
+    };
+    render(){
+        return(
+            <div>
+                <h3>Formulaire component</h3>
+                <form>
+                    <input type="number" placeholder="quantite" value="" onChange={(event)=> this.setState({quantity:5})}/>
+                    <input type="text" placeholder="article" value="" onChange={(event)=> console.log(event)}/>
+                    <button type="submit">ajouter</button>
+                </form>
+            </div>
+        );
+    }  
+}
 export default Form;
